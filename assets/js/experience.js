703bcc3fca7d0d04e413e40562800c3fdb3ed611
Serhii Trotsenko <sergh.vanger@gmail.com>
2021-07-15 22:40:37 +0300

    Fix experience feature

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,29 +1,30 @@
-const experienceContainer = document.getElementById('experience');
+function setExperience() {
+  const experienceContainer = document.getElementById('experience');
 
-console.log(experienceContainer);
-if (!experienceContainer) {
-  return;
+  if (!experienceContainer) {
+    return;
+  }
+
+  const experienceStartDate = new Date('2019-04-20T08:00:00').getTime();
+  const dateNow = new Date().getTime();
+  const difference = dateNow - experienceStartDate;
+
+  let seconds = Math.floor(difference / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = Math.floor(hours / 24);
+  let months = Math.floor(days / 30);
+  let years = Math.floor(days / 365);
+
+  seconds %= 60;
+  minutes %= 60;
+  hours %= 24;
+  days %= 30;
+  months %= 12;
+
+  const experience = `Experience - ${years} years, ${months} months and ${days} days`;
+
+  experienceContainer.innerHTML = experience;
 }
 
-const experienceStartDate = new Date('2019-04-20T08:00:00').getTime();
-const dateNow = new Date().getTime();
-const difference = dateNow - experienceStartDate;
-
-let seconds = Math.floor(difference / 1000);
-let minutes = Math.floor(seconds / 60);
-let hours = Math.floor(minutes / 60);
-let days = Math.floor(hours / 24);
-let months = Math.floor(days / 30);
-let years = Math.floor(days / 365);
-
-seconds %= 60;
-minutes %= 60;
-hours %= 24;
-days %= 30;
-months %= 12;
-
-const experience = `Experience - ${years} years, ${months} months and ${days} days`;
-
-console.log(experience);
-
-experienceContainer.innerHTML = experience;
+setExperience();
